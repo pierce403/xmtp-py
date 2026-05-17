5dd585f0630f4d110226db1086f2a436be8817db
Dean Pierce <pierce403@gmail.com>
2026-05-17 14:01:58 -0700

    Bump version to 0.1.6

--- sdks/agent-sdk/src/xmtp_agent/__init__.py
+++ sdks/agent-sdk/src/xmtp_agent/__init__.py
@@ -47,4 +47,4 @@ __all__ = [
     "serialize_message",
 ]
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
--- sdks/python-sdk/src/xmtp/__init__.py
+++ sdks/python-sdk/src/xmtp/__init__.py
@@ -26,4 +26,4 @@ __all__ = [
     "XmtpEnv",
 ]
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
